e95474792de70079f93a80b0c7fc05665605c78b
Mustafa Khan <makman09@gmail.com>
2017-10-21 15:07:13 -0700

    added calendar

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -6,7 +6,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: 'app.html',
+  styles: ['app.css']
 })
 export class MyApp {
   rootPage:any = TabsPage;
