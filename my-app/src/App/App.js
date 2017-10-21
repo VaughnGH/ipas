52dd7bdd90d494f0c6f393a07bf9cedc6b23e328
Corlin <tornato7@gmail.com>
2017-10-21 16:12:59 -0700

    Adding logo

--- my-app/src/App/App.js
+++ my-app/src/App/App.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 
 import DatePicker from 'material-ui/DatePicker';
 
-import logo from '../assets/logo.svg';
+import logo from '../assets/logo.png';
 import './App.css';
 
 class App extends Component {
 
-  
+
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">IPAS-135</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
