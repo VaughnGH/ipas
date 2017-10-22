794e6e86c1df6dc15ba33fa5cfeb70fe05aec5b8
Mustafa Khan <makman09@gmail.com>
2017-10-21 17:37:59 -0700

    finish overhall ui

--- my-app/src/App/App.js
+++ my-app/src/App/App.js
@@ -1,26 +1,139 @@
 import React, { Component } from 'react';
 
 import DatePicker from 'material-ui/DatePicker';
+import RaisedButton from 'material-ui/RaisedButton';
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import TextField from 'material-ui/TextField';
 
 import logo from '../assets/logo.svg';
 import './App.css';
 
+const styles = {
+  margin: 6,
+};
+
 class App extends Component {
 
-  
+  state = {}
+
+  setStartDate = (data: undefined, date: object) => {
+    this.setState({startDate: date.getTime()/1000})
+  }
+  setEndDate = (data:undefined, date: object) => {
+    this.setState({endDate: date.getTime()/1000})
+  }
+
+  setTemperature = (temperature) => this.setState({temperature: temperature})
+
+  submit = () => {
+    console.log(this.state)
+  }
+
+  temperatureButtons = () => {
+    return [
+              {temp: 'Cold', color: 'blue'},
+              {temp: 'Mild', color: 'grey'},
+              {temp: 'Hot', color: 'red'}
+            ].map( temp_object => {
+              return <RaisedButton 
+                        label={temp_object.temp} 
+                        onClick={() => this.setTemperature(temp_object.temp)}
+                        />
+            })
+
+  } 
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Marine Corps Logistics</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <DatePicker hintText="Start date" />
-        <DatePicker hintText="End date" />
+
+        <div className='form'>
+        <List>
+          <Subheader>Date</Subheader>
+          <ListItem
+            className='form-input'
+            children={
+              <DatePicker onChange={this.setStartDate} hintText="Start date" />
+            }
+          />
+          <ListItem
+            className='form-input'
+            children={
+              <DatePicker onChange={this.setEndDate} hintText="End date" />
+            }
+          />
+          <Subheader>Logistics</Subheader>
+          <ListItem
+            className='form-input'
+            children={
+              <TextField
+                floatingLabelText='Total PAX Estimate'
+                hintText="Amount of personnel attending"
+                type="number"
+                min="0"
+              />
+            }
+          />
+          <ListItem
+            className='form-input'
+            children={
+              <TextField
+                floatingLabelText="Average distance traveled per day"
+                hintText="Miles traveled per day"
+                type="number"
+                min="0"
+              />
+            }
+          />
+          <ListItem
+            className='form-input'
+            children={
+              <TextField
+                floatingLabelText="Total number of vehicles"
+                type="number"
+                min="0"
+              />
+            }
+          />
+          <ListItem
+            className='form-input'
+            children={
+              <TextField
+                floatingLabelText="Daily MRE count"
+                hintText="MREs needed per person per day"
+                type="number"
+                min="0"
+              />
+            }
+          />
+          <ListItem
+            className='form-input'
+            children={
+              <TextField
+                floatingLabelText="UGR count"
+                hintText="UGRs needed per person per day"
+                type="number"
+                min="0"
+              />
+            }
+          />
+        </List>
+        </div>
+
+        <div>
+          <RaisedButton
+            primary={true}
+            className='submit'
+            label="Submit Plan"
+            onClick={this.submit}
+          />
+        </div>
+
       </div>
     );
   }
